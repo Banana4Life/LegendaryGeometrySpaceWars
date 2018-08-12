8b9c34e03586b4cd44047f0a495a22609c9dc613
Anselm Brehme <development.anselm@breh.me>
2018-08-12 23:36:02 +0200

    try border

--- source/Grid.js
+++ source/Grid.js
@@ -5,20 +5,21 @@
 
 		let states = Object.assign({}, data);
 
+		this.deathRing = 1;
+		this.updateDeath = true;
+
 
 		this._frames = 0;
 		this._last   = null;
 		this._start  = null;
 
-
-		let divisions = states.divisions || 100;
-		let size      = states.size || 1000;
-
+		this.divisions = states.divisions || 100;
+		this.size      = states.size || 1000;
 
 		this.player = player || null;
 
 		let pMaterial = new THREE.PointsMaterial({
-			color: 0x000055,
+			vertexColors: THREE.VertexColors,
 			size: 70,
 			map: new THREE.TextureLoader().load("images/particle.png"),
 			blending: THREE.AdditiveBlending,
@@ -28,8 +29,10 @@
 
 		this.geometry = new THREE.Geometry();
 		let zero = new THREE.Vector3(0, 0, 0);
-		for (let i = 0; i < divisions * divisions; i++) {
+		let defColor = new THREE.Color(0x000055);
+		for (let i = 0; i < this.divisions * this.divisions; i++) {
 			this.geometry.vertices.push(zero.clone());
+			this.geometry.colors.push(defColor);
 		}
 
 		this.object = new THREE.Points(this.geometry, pMaterial);
@@ -37,8 +40,8 @@
 
 		this.geometry.vertices.forEach((pos, p) => {
 
-			let z = (-1 / 2 * size) + (size / divisions) * (p % divisions);
-			let x = (-1 / 2 * size) + (size / divisions) * Math.floor(p / divisions);
+			let z = (-1 / 2 * this.size) + (this.size / this.divisions ) * (p % this.divisions);
+			let x = (-1 / 2 * this.size) + (this.size / this.divisions ) * Math.floor(p / this.divisions);
 
 			pos._x = x;
 			pos._z = z;
@@ -49,10 +52,14 @@
 
 
 		this.geometry.verticesNeedUpdate = true;
+		this.geometry.colorsNeedUpdate = true;
 
+
+		this.object.userData = { entity: this};
 
 
 		scene.add(this.object);
+
 
 		this.object.name = 'Grid';
 
@@ -62,6 +69,23 @@
 	Grid.prototype = {
 
 		update: function() {
+
+			if (this.updateDeath) {
+				this.updateDeath = false;
+				console.log("DeathRing is: " + this.deathRing);
+				let color = new THREE.Color(0xff0000);
+				this.geometry.vertices.forEach((pos, i) => {
+					console.log(pos._x);
+
+					if (500 - Math.abs(pos._x) <= this.deathRing) {
+						this.geometry.colors[i] = color;
+					} else if (500 - Math.abs(pos._z) <= this.deathRing) {
+						this.geometry.colors[i] = color;
+					}
+				});
+
+				this.geometry.colorsNeedUpdate = true;
+			}
 
 			let clock = Date.now();
 
